fa3736f3c16b547e09fafa16a20b0adbbc6acc22
UmutBasarann <30317329+UmutBasarann@users.noreply.github.com>
2022-02-20 00:00:10 +0300

    Damage Dealing Added

---
+++ MultiplayerTankGame/Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+namespace Combat
+{
+    public class Health : NetworkBehaviour
+    {
+        #region EventHandler || Action
+
+        public event Action ServerOnDie;
+
+        #endregion
+        
+        #region Childs
+
+        [SerializeField] 
+        private int _maxHealth = 100;
+
+        #endregion
+
+        #region Fields
+
+        [SyncVar] 
+        private int _currentHealth;
+
+        #endregion
+
+        #region Server
+
+        public override void OnStartServer()
+        {
+            _currentHealth = _maxHealth;
+        }
+
+        [Server]
+        public void DealDamage(int damageAmount)
+        {
+            if (_currentHealth == 0)
+            {
+                return;
+            }
+
+            _currentHealth = Mathf.Max(_currentHealth - damageAmount, 0);
+
+            if (_currentHealth != 0)
+            {
+                return;
+            }
+            
+            ServerOnDie?.Invoke();
+            
+            Debug.Log("We died");
+        }
+
+        #endregion
+
+        #region Client
+
+        
+
+        #endregion
+    }
+}
--- MultiplayerTankGame/Assets/Scripts/Units/UnitProjectile.cs
+++ MultiplayerTankGame/Assets/Scripts/Units/UnitProjectile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Combat;
 using Mirror;
 using UnityEngine;
 
@@ -11,6 +12,7 @@ namespace Units
         [SerializeField] private Rigidbody _rigidbody = null;
         [SerializeField] private float _destroyAfterSeconds = 5f;
         [SerializeField] private float _launchForce = 10f;
+        [SerializeField] private int _damageToDeal = 20;
 
         #endregion
 
@@ -29,6 +31,25 @@ namespace Units
         private void DestroySelf()
         {
             NetworkServer.Destroy(gameObject);
+        }
+
+        [Server]
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent<NetworkIdentity>(out var networkIdentity))
+            {
+                if (networkIdentity.connectionToClient == connectionToClient)
+                {
+                    return;
+                }
+            }
+
+            if (other.TryGetComponent<Health>(out var health))
+            {
+                health.DealDamage(_damageToDeal);
+            }
+            
+            DestroySelf();
         }
 
         #endregion
