cbe020722f910b7a8cf27678cfab97ff6ede31d7
UmutBasarann <30317329+UmutBasarann@users.noreply.github.com>
2022-02-21 00:10:56 +0300

    Health Bar Face Camera Added

---
+++ MultiplayerTankGame/Assets/Scripts/Cameras/FaceCamera.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Cameras
+{
+    public class FaceCamera : MonoBehaviour
+    {
+        #region Fields
+
+        private Transform _mainCameraTransform;
+
+        #endregion
+
+        #region Awake || Start || Update
+
+        private void Start()
+        {
+            _mainCameraTransform = Camera.main.transform;
+        }
+
+        private void LateUpdate()
+        {
+            transform.LookAt(transform.position + _mainCameraTransform.rotation * Vector3.forward, _mainCameraTransform.rotation * Vector3.up);
+        }
+
+        #endregion
+    }
+}
